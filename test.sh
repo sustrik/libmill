d317fdeef8f84372c2fd0d1b810f80544ae05a6d
Martin Sustrik <sustrik@250bpm.com>
2014-05-30 07:18:06 +0200

    tcpsocket test added
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 ./mill sleep.mill
 gcc -o sleep sleep.c mill.c -luv -g -O0
 ./sleep
+./mill tcpsocket.mill
+gcc -o sleep tcpsocket.c mill.c -luv -g -O0
+./tcpsocket
 
 ./mill hello.mill
 gcc -o hello hello.c mill.c -luv -g -O0
