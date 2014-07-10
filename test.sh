3a6b8a6521a6e83d01141a5a06d5f6b42625e49f
Martin Lucina <martin@lucina.net>
2014-07-10 12:20:54 +0200

    Separate code generation phase from end-user use
    - Mill source itself (.dna, clexer) is moved to gen/
    - configure gets an option --enable-gen, which checks for dependencies
      and allows regeneration of MIll itself from files in gen.
    - end-user dependencies are thus only Ruby and libuv.

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-./ribosome --rna mill.dna > ./mill
-chmod +x ./mill
-./mill --system stdmill.mh
-./mill --system stdmill.mc
-./mill test.mc
-gcc -o test mill.c stdmill.c test.c -luv -g -O0
-./test
