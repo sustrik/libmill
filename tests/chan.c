26fdd0723414ebdd32bef2a6954f0e98a7fe7078
Martin Sustrik <sustrik@250bpm.com>
2015-04-25 04:40:46 +0200

    Minor improvement to chan test
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/chan.c
+++ tests/chan.c
@@ -12,20 +12,17 @@ void sender(chan ch, int doyield, void *val) {
 
 int main() {
     chan ch;
-    void *val;
 
     /* Receiver waits for sender. */
     ch = chmake();
     go(sender(ch, 1, (void*)333));
-    val = chr(ch);
-    assert(val == (void*)333);
+    assert(chr(ch) == (void*)333);
     chclose(ch);
 
     /* Sender waits for receiver. */
     ch = chmake();
     go(sender(ch, 0, (void*)444));
-    val = chr(ch);
-    assert(val == (void*)444);
+    assert(chr(ch) == (void*)444);
     chclose(ch);
 
     return 0;
