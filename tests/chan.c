a3b8d58ed4efe277f4494d4d486b26478e0f9974
Martin Sustrik <sustrik@250bpm.com>
2015-04-25 04:39:09 +0200

    chan test improved
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/chan.c
+++ tests/chan.c
@@ -3,18 +3,31 @@
 #include <assert.h>
 #include "../mill.h"
 
-void foo(chan ch) {
-    yield();
-    chs(ch, (void*)333);
+void sender(chan ch, int doyield, void *val) {
+    if(doyield)
+        yield();
+    chs(ch, val);
     chclose(ch); 
 }
 
 int main() {
-    chan ch = chmake();
-    go(foo(ch));
-    void *val = chr(ch);
+    chan ch;
+    void *val;
+
+    /* Receiver waits for sender. */
+    ch = chmake();
+    go(sender(ch, 1, (void*)333));
+    val = chr(ch);
     assert(val == (void*)333);
     chclose(ch);
+
+    /* Sender waits for receiver. */
+    ch = chmake();
+    go(sender(ch, 0, (void*)444));
+    val = chr(ch);
+    assert(val == (void*)444);
+    chclose(ch);
+
     return 0;
 }
 
