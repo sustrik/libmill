c7150ee7766cba4c1d9c4b9a3eb6591d805aef3b
Martin Sustrik <sustrik@250bpm.com>
2015-04-30 08:37:33 +0200

    Some tests moved from choose.c to chan.c
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/chan.c
+++ tests/chan.c
@@ -10,20 +10,48 @@ void sender(chan ch, int doyield, void *val) {
     chclose(ch); 
 }
 
-int main() {
-    chan ch;
-
-    /* Receiver waits for sender. */
-    ch = chmake();
-    go(sender(chdup(ch), 1, (void*)333));
-    assert(chr(ch) == (void*)333);
+void sender1(chan ch, void *val) {
+    chs(ch, val);
     chclose(ch);
+}
+
+void receiver1(chan ch, void *expected) {
+    void *val = chr(ch);
+    assert(val == expected);
+    chclose(ch);
+}
+
+int main() {
+    /* Receiver waits for sender. */
+    chan ch1 = chmake();
+    go(sender(chdup(ch1), 1, (void*)333));
+    assert(chr(ch1) == (void*)333);
+    chclose(ch1);
 
     /* Sender waits for receiver. */
-    ch = chmake();
-    go(sender(chdup(ch), 0, (void*)444));
-    assert(chr(ch) == (void*)444);
-    chclose(ch);
+    chan ch2 = chmake();
+    go(sender(chdup(ch2), 0, (void*)444));
+    assert(chr(ch2) == (void*)444);
+    chclose(ch2);
+
+    /* Test two simultaneous senders. */
+    void *val;
+    chan ch3 = chmake();
+    go(sender1(chdup(ch3), (void*)888));
+    go(sender1(chdup(ch3), (void*)999));
+    val = chr(ch3);
+    assert(val == (void*)888);
+    val = chr(ch3);
+    assert(val == (void*)999);
+    chclose(ch3);
+
+    /* Test two simultaneous receivers. */
+    chan ch4 = chmake();
+    go(receiver1(chdup(ch4), (void*)333));
+    go(receiver1(chdup(ch4), (void*)444));
+    chs(ch4, (void*)333);
+    chs(ch4, (void*)444);
+    chclose(ch4);
 
     return 0;
 }
