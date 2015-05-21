b7382d155f870f6f405bfc7f3363cdc5ae4ca103
Martin Sustrik <sustrik@250bpm.com>
2015-05-21 20:35:31 +0200

    slist implementation added
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

---
+++ slist.c
@@ -0,0 +1,85 @@
+/*
+
+  Copyright (c) 2015 Martin Sustrik
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom
+  the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.
+
+*/
+
+#include "slist.h"
+
+#include <assert.h>
+#include <stddef.h>
+
+void mill_slist_init(struct mill_slist *self) {
+    self->first = NULL;
+    self->last = NULL;
+}
+
+void mill_slist_term(struct mill_slist *self) {
+    assert(!self->first);
+    assert(!self->last);
+}
+
+int mill_slist_empty(struct mill_slist *self) {
+    return self->first ? 1 : 0;
+}
+
+struct mill_slist_item *mill_slist_begin(struct mill_slist *self) {
+    return self->first;
+}
+
+struct mill_slist_item *mill_slist_end(struct mill_slist *self) {
+    return NULL;
+}
+
+struct mill_slist_item *mill_slist_next(struct mill_slist *self,
+      struct mill_slist_item *it) {
+    return it->next;
+}
+
+void mill_slist_push(struct mill_slist *self, struct mill_slist_item *item) {
+    item->next = self->first;
+    self->first = item;
+    if(!self->last)
+        self->last = item;
+}
+
+void mill_slist_push_back(struct mill_slist *self,
+      struct mill_slist_item *item) {
+    item->next = NULL;
+    if(!self->last) {
+        self->last = item;
+        self->first = item;
+        return;
+    }
+    self->last->next = item;
+    self->last = item;
+}
+
+struct mill_slist_item *mill_slist_pop(struct mill_slist *self) {
+    if(!self->first)
+        return NULL;
+    struct mill_slist_item *it = self->first;
+    self->first = self->first->next;
+    if(!self->first)
+        self->last = NULL;
+    return it;
+}
+
---
+++ slist.h
@@ -0,0 +1,68 @@
+/*
+
+  Copyright (c) 2015 Martin Sustrik
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom
+  the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.
+
+*/
+
+#ifndef MILL_SLIST_INCLUDED
+#define MILL_SLIST_INCLUDED
+
+struct mill_slist_item {
+    struct mill_slist_item *next;
+};
+
+struct mill_slist {
+    struct mill_slist_item *first;
+    struct mill_slist_item *last;
+};
+
+/* Initialise the list. To statically initialise the list use = {0}. */
+void mill_slist_init(struct mill_slist *self);
+
+/* Terminates the list. Note that all items must be removed before the
+   termination. */
+void mill_slist_term(struct mill_slist *self);
+
+/* Returns 1 is list has zero items, 0 otherwise. */
+int mill_slist_empty(struct mill_slist *self);
+
+/* Returns iterator to the first item in the list. */
+struct mill_slist_item *mill_slist_begin(struct mill_slist *self);
+
+/* Returns iterator to one past the last item in the list. */
+struct mill_slist_item *mill_slist_end(struct mill_slist *self);
+
+/* Returns iterator to one past the item pointed to by 'it'. */
+struct mill_list_item *mill_list_next(struct mill_slist *self,
+    struct mill_slist_item *it);
+
+/* Push the item to the beginning of the list. */
+void mill_slist_push(struct mill_slist *self, struct mill_slist_item *item);
+
+/* Push the item to the end of the list. */
+void mill_slist_push_back(struct mill_slist *self,
+    struct mill_slist_item *item);
+
+/* Pop an item from the beginning of the list. */
+struct mill_slist_item *mill_slist_pop(struct mill_slist *self);
+
+#endif
+
