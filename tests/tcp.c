08e34d263b8d604f5160d3d7b97b69f19a3782b1
Martin Sustrik <sustrik@250bpm.com>
2015-05-23 19:54:38 +0200

    tcp test fixed
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/tcp.c
+++ tests/tcp.c
@@ -31,20 +31,13 @@ void client(void) {
     tcpsock cs = tcpconnect("127.0.0.1:5555");
     assert(cs);
 
-    tcpsend(cs, "ABC", 3);
+    char buf[16];
+    ssize_t sz = tcprecv(cs, buf, 3);
+    assert(buf[0] == 'A' && buf[1] == 'B' && buf[2] == 'C');
+
+    tcpsend(cs, "123\n45\n6789", 11);
     int rc = tcpflush(cs);
     assert(rc == 0);
-
-    char buf[16];
-    ssize_t sz = tcprecvuntil(cs, buf, sizeof(buf), '\n');
-    assert(sz == 4);
-    assert(buf[0] == '1' && buf[1] == '2' && buf[2] == '3' && buf[3] == '\n');
-    sz = tcprecvuntil(cs, buf, sizeof(buf), '\n');
-    assert(sz == 3);
-    assert(buf[0] == '4' && buf[1] == '5' && buf[2] == '\n');
-    sz = tcprecvuntil(cs, buf, 3, '\n');
-    assert(sz == 0);
-    assert(buf[0] == '6' && buf[1] == '7' && buf[2] == '8');
 
     tcpclose(cs);
 }
@@ -57,13 +50,20 @@ int main() {
 
     tcpsock as = tcpaccept(ls);
 
-    char buf[16];
-    ssize_t sz = tcprecv(as, buf, 3);
-    assert(buf[0] == 'A' && buf[1] == 'B' && buf[2] == 'C');
-
-    tcpsend(as, "123\n45\n6789", 9);
+    tcpsend(as, "ABC", 3);
     int rc = tcpflush(as);
     assert(rc == 0);
+
+    char buf[16];
+    ssize_t sz = tcprecvuntil(as, buf, sizeof(buf), '\n');
+    assert(sz == 4);
+    assert(buf[0] == '1' && buf[1] == '2' && buf[2] == '3' && buf[3] == '\n');
+    sz = tcprecvuntil(as, buf, sizeof(buf), '\n');
+    assert(sz == 3);
+    assert(buf[0] == '4' && buf[1] == '5' && buf[2] == '\n');
+    sz = tcprecvuntil(as, buf, 3, '\n');
+    assert(sz == 0);
+    assert(buf[0] == '6' && buf[1] == '7' && buf[2] == '8');
 
     tcpclose(as);
     tcpclose(ls);
