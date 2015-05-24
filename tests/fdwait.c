1982b4854315854a3cd95ec263a4cff69ac44195
Martin Sustrik <sustrik@250bpm.com>
2015-05-24 13:37:44 +0200

    fdwait test rewritten to use socketpair
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/fdwait.c
+++ tests/fdwait.c
@@ -23,14 +23,47 @@
 */
 
 #include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include "../libmill.h"
 
-int main() {
+static uint64_t now() {
+    struct timeval tv;
+    int rc = gettimeofday(&tv, NULL);
+    assert(rc == 0);
+    return ((uint64_t)tv.tv_sec) * 1000 + (((uint64_t)tv.tv_usec) / 1000);
+}
 
-    /* Wait while stdout is writeable. */
-    int rc = fdwait(1, FDW_OUT);
+int main() {
+    /* Create a pair of file descriptors for testing. */
+    int fds[2];
+    int rc = socketpair(AF_UNIX, SOCK_STREAM, 0, fds);
+    assert(rc == 0);
+
+    /* Check whether the pipe is writeable. */
+    rc = fdwait(fds[0], FDW_OUT);
     assert(rc & FDW_OUT);
     assert(!(rc & ~FDW_OUT));
+
+#if 0
+    /* Check the same with timeout. */
+    rc = fdwait(fds[0], FDW_OUT, 100);
+    assert(rc & FDW_OUT);
+    assert(!(rc & ~FDW_OUT));
+
+    /* Check that the pipe is not readable. */
+    uint64_t ms = now();
+    rc = fdwait(fds[0], FDW_IN, 100);
+    assert(rc == 0);
+    ms = now() - ms;
+    assert(ms > 90 && ms < 110);
+#endif
+
+    close(fds[0]);
+    close(fds[1]);
 
     return 0;
 }
