dee6cc662bb85d8f5694cfa6bfc7cc9feead38c0
Martin Sustrik <sustrik@250bpm.com>
2015-06-16 06:14:52 +0200

    ABI version bumped
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- libmill.h
+++ libmill.h
@@ -43,7 +43,7 @@
 #define MILL_VERSION_CURRENT 4
 
 /*  The latest revision of the current interface. */
-#define MILL_VERSION_REVISION 2
+#define MILL_VERSION_REVISION 3
 
 /*  How many past interface versions are still supported. */
 #define MILL_VERSION_AGE 0
