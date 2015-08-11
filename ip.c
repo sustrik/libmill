2303347bb4c73dd6a156e1cf2e161ff57e4313d0
Martin Sustrik <sustrik@250bpm.com>
2015-08-11 20:11:44 +0200

    DNS resolver for Linux implemented
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- ip.c
+++ ip.c
@@ -22,17 +22,24 @@
 
 */
 
+#if defined __linux__
+#define _GNU_SOURCE
+#include <netdb.h>
+#include <sys/eventfd.h>
+#endif
+
 #include "ip.h"
 #include "libmill.h"
 #include "utils.h"
 
 #include <arpa/inet.h>
 #include <errno.h>
+#include <ifaddrs.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <ifaddrs.h>
+#include <unistd.h>
 
 MILL_CT_ASSERT(sizeof(ipaddr) >= sizeof(struct sockaddr_in));
 MILL_CT_ASSERT(sizeof(ipaddr) >= sizeof(struct sockaddr_in6));
@@ -215,8 +222,103 @@ ipaddr iplocal(const char *name, int port, int mode) {
     return addr;
 }
 
+#if defined __linux__
+static void mill_getaddrinfo_a_done(union sigval sval) {
+    uint64_t i = 1;
+    ssize_t sz = write(sval.sival_int, &i, sizeof(i));
+    mill_assert(sz == sizeof(i));
+}
+#endif
+
 ipaddr ipremote(const char *name, int port, int mode, int64_t deadline) {
-    return mill_ipliteral(name, port, mode);
-    /* TODO: Do a DNS query here. */
+    ipaddr addr = mill_ipliteral(name, port, mode);
+#if !defined __linux__
+    return addr;
+#else
+    if(errno == 0)
+       return addr;
+    /* Let's do asynchronous DNS query here. */
+    int efd = eventfd(0, 0);
+    if(mill_slow(efd < 0))
+        return addr;
+    struct addrinfo request;
+    memset(&request, 0, sizeof(request));
+    request.ai_family = AF_UNSPEC;
+    request.ai_socktype = SOCK_STREAM;
+    struct gaicb gcb;
+    memset(&gcb, 0, sizeof(gcb));
+    gcb.ar_name = name;
+    gcb.ar_service = NULL;
+    gcb.ar_request = &request;
+    gcb.ar_result = NULL;
+    struct sigevent sev;
+    memset(&sev, 0, sizeof(sev));
+    /* The event will be delivered using a new thread rather than by a signal
+       running of one of the coroutines' stack and possibly breaking it. */
+    sev.sigev_notify = SIGEV_THREAD;
+    sev.sigev_notify_function = mill_getaddrinfo_a_done;
+    sev.sigev_value.sival_int = efd;
+    struct gaicb *pgcb = &gcb;
+    int rc = getaddrinfo_a(GAI_NOWAIT, &pgcb, 1, &sev);
+    if(mill_slow(rc != 0)) {
+        if(rc == EAI_AGAIN || rc == EAI_MEMORY) {
+            errno = ENOMEM;
+            return addr;
+        }
+        mill_assert(0);
+    }
+    rc = fdwait(efd, FDW_IN, deadline);
+    mill_assert(rc == FDW_IN); /* TODO: Handle deadline! */
+    close(efd);
+    rc = gai_error(&gcb);
+    if(rc != 0) {
+        errno = EINVAL;
+        return addr;
+    }
+    struct addrinfo *ipv4 = NULL;
+    struct addrinfo *ipv6 = NULL;
+    struct addrinfo *it = gcb.ar_result;
+    while(it) {
+        if(!ipv4 && it->ai_family == AF_INET)
+            ipv4 = it;
+        if(!ipv6 && it->ai_family == AF_INET6)
+            ipv6 = it;
+        if(ipv4 && ipv6)
+            break;
+        it = it->ai_next;
+    }
+    switch(mode) {
+    case IPADDR_IPV4:
+        ipv6 = NULL;
+        break;
+    case IPADDR_IPV6:
+        ipv4 = NULL;
+        break;
+    case 0:
+    case IPADDR_PREF_IPV4:
+        if(ipv4)
+           ipv6 = NULL;
+        break;
+    case IPADDR_PREF_IPV6:
+        if(ipv6)
+           ipv4 = NULL;
+        break;
+    default:
+        mill_assert(0);
+    }
+    if(ipv4) {
+        struct sockaddr_in *inaddr = (struct sockaddr_in*)&addr;
+        memcpy(inaddr, ipv4->ai_addr, sizeof (struct sockaddr_in));
+        inaddr->sin_port = htons(port);
+    }
+    if(ipv6) {
+        struct sockaddr_in6 *inaddr = (struct sockaddr_in6*)&addr;
+        memcpy(inaddr, ipv6->ai_addr, sizeof (struct sockaddr_in6));
+        inaddr->sin6_port = htons(port);
+    }
+    freeaddrinfo(gcb.ar_result);
+    errno = 0;
+    return addr;
+#endif
 }
 
