be5d2445bbbbe45c2943226c68fac15236de777b
Martin Sustrik <sustrik@250bpm.com>
2015-11-26 02:17:34 +0100

    mill_fdwait split into mechanism specific and generic stuff
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- poller.c
+++ poller.c
@@ -24,8 +24,19 @@
 
 #include <stdint.h>
 
+#include "cr.h"
 #include "libmill.h"
 #include "list.h"
+
+/* Forward declarations for the functions implemented by specific poller
+   mechanisms (poll, epoll, kqueue). */
+static void mill_poller_init(void);
+static void mill_poller_add(int fd, int events);
+static void mill_poller_rm(int fd, int events);
+static void mill_poller_wait(int block);
+
+/* If 1, mill_poller_init was already called. */
+static int mill_poller_initialised = 0;
 
 /* Global linked list of all timers. The list is ordered.
    First timer to be resume comes first and so on. */
@@ -34,6 +45,58 @@ static struct mill_list mill_timers = {0};
 /* Pause current coroutine for a specified time interval. */
 void mill_msleep(int64_t deadline, const char *current) {
     mill_fdwait(-1, 0, deadline, current);
+}
+
+int mill_fdwait(int fd, int events, int64_t deadline, const char *current) {
+    /* If required, start waiting for the timeout. */
+    if(deadline >= 0) {
+        mill_running->u_fdwait.expiry = deadline;
+        /* Move the timer into the right place in the ordered list
+           of existing timers. TODO: This is an O(n) operation! */
+        struct mill_list_item *it = mill_list_begin(&mill_timers);
+        while(it) {
+            struct mill_fdwait *timer = mill_cont(it, struct mill_fdwait, item);
+            /* If multiple timers expire at the same momemt they will be fired
+               in the order they were created in (> rather than >=). */
+            if(timer->expiry > mill_running->u_fdwait.expiry)
+                break;
+            it = mill_list_next(it);
+        }
+        mill_list_insert(&mill_timers, &mill_running->u_fdwait.item, it);
+    }
+    /* If required, start waiting for the file descriptor. */
+    if(fd >= 0) {
+        if(mill_slow(!mill_poller_initialised)) {
+            mill_poller_init();
+            mill_poller_initialised = 1;
+        }
+        mill_poller_add(fd, events);
+    }
+    /* Do actual waiting. */
+    mill_running->state = fd < 0 ? MILL_MSLEEP : MILL_FDWAIT;
+    mill_set_current(&mill_running->debug, current);
+    int rc = mill_suspend();
+    /* Handle file descriptor events. */
+    if(rc >= 0) {
+        if(deadline >= 0)
+            mill_list_erase(&mill_timers, &mill_running->u_fdwait.item);
+        return rc;
+    }
+    /* Handle the timeout. Clean-up the pollset. */
+    if(fd >= 0) {
+        /* No need to initialise the poller here. It must have been already
+           initialised above. */
+        mill_poller_rm(fd, events);
+    }
+    return 0;
+}
+
+void mill_wait(int block) {
+    if(mill_slow(!mill_poller_initialised)) {
+        mill_poller_init();
+        mill_poller_initialised = 1;
+    }
+    mill_poller_wait(block);
 }
 
 /* Include the poll-mechanism-specific stuff. */
@@ -46,8 +109,9 @@ void mill_msleep(int64_t deadline, const char *current) {
 #elif defined MILL_POLL
 #include "poll.inc"
 /* Defaults. */
-#elif 0 && defined __linux__
+#elif defined __linux__
 #include "epoll.inc"
 #else
 #include "poll.inc"
 #endif
+
