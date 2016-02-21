4a8787340801d5a71363a448905e5262f8751ccc
Martin Sustrik <sustrik@250bpm.com>
2016-02-21 04:59:32 +0100

    Improve mfork() test
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/go.c
+++ tests/go.c
@@ -25,6 +25,7 @@
 #include <errno.h>
 #include <assert.h>
 #include <stdio.h>
+#include <sys/wait.h>
 
 #include "../libmill.h"
 
@@ -62,6 +63,12 @@ int main() {
     /* Try to fork the process. */
     pid_t pid = mfork();
     assert(pid != -1);
+    if(pid > 0) {
+        int status;
+        pid = waitpid(pid, &status, 0);
+        assert(pid != -1);
+        assert(WIFEXITED(status));
+    }
 
     return 0;
 }
