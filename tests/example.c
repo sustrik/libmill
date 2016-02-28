d0fa384bc23d6f70ff5f2bda19204b07445e366f
Martin Sustrik <sustrik@250bpm.com>
2016-02-28 08:14:10 +0100

    Fix 'example' test
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/example.c
+++ tests/example.c
@@ -30,7 +30,7 @@ coroutine void worker(int count, const char *text) {
     int i;
     for(i = 0; i != count; ++i) {
         printf("%s\n", text);
-        msleep(10);
+        msleep(now() + 10);
     }
 }
 
@@ -38,7 +38,7 @@ int main() {
     go(worker(4, "a"));
     go(worker(2, "b"));
     go(worker(3, "c"));
-    msleep(100);
+    msleep(now () + 100);
     return 0;
 }
 
