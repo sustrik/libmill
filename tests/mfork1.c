903858e1c1b0582d550765cb85f0340963492d3d
Martin Sustrik <sustrik@250bpm.com>
2016-03-06 06:12:55 +0100

    mfork1 test added
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

---
+++ tests/mfork1.c
@@ -0,0 +1,69 @@
+/*
+
+  Copyright (c) 2015 Martin Sustrik
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom
+  the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.
+
+*/
+
+#include <errno.h>
+#include <assert.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+#include "../libmill.h"
+
+int forked = 0;
+int worker_running = 0;
+
+coroutine void worker(void) {
+    while(1) {
+        if(forked)
+            break;
+        yield();
+    }
+    worker_running = 1;
+}
+
+int main() {
+    /* Start second coroutine before forking. */
+    go(worker());
+    /* Fork. */
+    pid_t pid = mfork();
+    assert(pid != -1);
+    /* Parent waits for the child. */
+    if(pid > 0) {
+        int status;
+        pid = waitpid(pid, &status, 0);
+        assert(pid != -1);
+        assert(WIFEXITED(status));
+        assert(WEXITSTATUS(status) == 0);
+        return 0;
+    }
+
+    /* Child tries to make sure that there's only one coroutine running. */
+    forked = 1; 
+    int i;
+    for(i = 0; i != 20; ++i)
+        yield();
+    assert(!worker_running);
+
+    return 0;
+}
+
